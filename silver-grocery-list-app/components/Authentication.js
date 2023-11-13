ffa46793b4e9c1607d5f276f9ac68fad4ee6cb7a
aupreti1 <anaupreti@gmail.com>
2023-11-12 20:30:37 -0500

    Adding Firebase

--- silver-grocery-list-app/components/Authentication.js
+++ silver-grocery-list-app/components/Authentication.js
@@ -4,3 +4,31 @@ export const AuthContext = createContext({
     hasUser: false,
     setUser: () => {}
 });
+
+// import React from 'react';
+// import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+
+// const auth = getAuth();
+
+// export function AuthContext() {
+//   const [user, setUser] = React.useState(User);
+
+//   React.useEffect(() => {
+//     const unsubscribeFromAuthStatuChanged = onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         // User is signed in, see docs for a list of available properties
+//         // https://firebase.google.com/docs/reference/js/firebase.User
+//         setUser(user);
+//       } else {
+//         // User is signed out
+//         setUser(undefined);
+//       }
+//     });
+
+//     return unsubscribeFromAuthStatuChanged;
+//   }, []);
+
+//   return {
+//     user
+//   };
+// }
---
+++ silver-grocery-list-app/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import Constants from 'expo-constants';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
+  authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
+  projectId: Constants.expoConfig?.extra?.firebaseProjectId,
+  storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId,
+  appId: Constants.expoConfig?.extra?.firebaseAppId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
